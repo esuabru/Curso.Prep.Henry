fc662312b544ea9a27a04ad1cdcb029e87b5efc5
esteban <esuabru@gmail.com>
2020-08-27 16:24:39 -0400

    sandbox

---
+++ 02-JS-I/homework/sandbox.js
@@ -0,0 +1,24 @@
+
+	* Variables:
+	Son elementos de programaciòn de javascript que definen valores y conceptos que luego son utilizados para interactuar en el código. 
+	Es como ponerle nombre a una flor. El las letras son flores, pero cuando les pones nombre; es decir, cuando las haces variables, es cuando se transforman en algo más.
+	Una variable es un elemento único y fundamental.
+
+	* Strings
+	Imagina que los carácteres son como vikingos. Cada uno de ellos tiene un valor, pero todos juntos son poderosos. Cuando se suben a sus barcos son más que solo caracteres. Son un strings, es decir, un elemento único y completo, indivisible, que interactua con el código de forma conjunta.
+
+	* Funciones (argumentos, `return`)
+
+las funciones son conjuros que permiten que sucedan cosas, con funciones puedes decirle a javascript que una variable tiene que tener cierto comportamiento cuando tenga un valor en específico, .
+El argumento es la sentencia en donde situamos de que variables hablaremos y como se llama el nombre de esa función. 	
+	El Return se utiliza cuando queremos que el programa nos devuelva cierta informaci+on, puede ser nueva info o info anterior. Pero se utiliza siempre para que nos muestre datos en la pantalla.
+
+	
+ Declaraciones `if`
+
+ La delcaración if es el código mágico para señalar causalidad. Es decir,msi yo quiero que suceda una acción solo cuando pase algo es especìfico, entonces estoy señalando una causa.
+ Por ejemplo, un formulario donde solo sean aceptados los mayores de edad. Entonces if edad > 18 return true
+	* Valores booleanos (`true`, `false`)
+	finalmente, los còdigos booleanos son puertas que vamos a utilizar para construir nuestros caminos, los utilizaremos para definir si algo es verdadero o falso, es decir, si es la respuesta que necesitamos o no. Esto tiene miles de usos pero son la pieza funadamental para lograr còdigos complejos.
+	Los valores se escriben en inglés.
+	
